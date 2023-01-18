6fba851ce54b32cefd75d276aa26bbf4dc9c0961
Huichoman <rockbotica@gmail.com>
2023-01-17 19:49:45 -0600

    Add mod.console.js

---
+++ mod-console.js
@@ -0,0 +1,7 @@
+console.log("Message with console.log");
+
+console.warn("Message with console.warn");
+
+console.error("Message with console.error");
+
+console.error(new Error("Oops! new error"));
